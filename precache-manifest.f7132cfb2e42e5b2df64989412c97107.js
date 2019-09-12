e94fad9d4bb6939833f652a88af0b216ff324152
Ratsamee <jayin@hotmail.com>
2019-09-12 16:01:54 +1000

    Updates

---
+++ precache-manifest.f7132cfb2e42e5b2df64989412c97107.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e08a5d914d23dcb19e116a3d93b9351",
+    "url": "/bookstore/index.html"
+  },
+  {
+    "revision": "970a10386bade1ae0b1d",
+    "url": "/bookstore/static/css/2.7987673b.chunk.css"
+  },
+  {
+    "revision": "0650a69f6fdcb362e142",
+    "url": "/bookstore/static/css/main.a570f769.chunk.css"
+  },
+  {
+    "revision": "970a10386bade1ae0b1d",
+    "url": "/bookstore/static/js/2.4c6ff18b.chunk.js"
+  },
+  {
+    "revision": "0650a69f6fdcb362e142",
+    "url": "/bookstore/static/js/main.9afbd6ee.chunk.js"
+  },
+  {
+    "revision": "b2ab4dfa036b00d5ed61",
+    "url": "/bookstore/static/js/runtime~main.d4e56629.js"
+  }
+]);
